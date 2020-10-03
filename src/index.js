60a00ac02c4a003ae682095ef7c43b20a1cfd9ad
SlavaFrolov <7566724@gmail.com>
2020-10-03 13:18:19 +0300

    init: create functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
+function check (array){
+  if (array.length < 4)
+    return 0;
+  for (let i = 0; i < array.length; i++){
+    if (array[i] === null)
+      return 0;
+  }
+  return 1;
+}
 
 exports.min = function min (array) {
-  return 0;
+  if(array === undefined || check (array) === 0 )
+    return 0;
+  let min = array[0];
+  for (let i = 1; i < array.length; i++){
+    if (array[i] < min)
+      min = array[i];
+  }
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  
+  if(array === undefined || check (array) === 0)
+    return 0;
+  let max = array[0];
+  for (let i = 1; i < array.length; i++){
+    if (array[i] > max)
+      max = array[i];
+  }
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(array === undefined || check (array) === 0)
+    return 0;
+  let sum = 0;
+  for (let i = 0; i < array.length; i++){
+    sum += array[i];
+  }
+  return sum / array.length;
 }
